16a14955bcddcfb9fe8b8b80306248ad40fc4032
Franco <franco.fantillo@gmail.com>
2023-02-09 10:01:57 -1000

    Initial Commit

---
+++ SqaureTest/SqaureTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SqaureTestApp.swift
+//  SqaureTest
+//
+//  Created by Franco Fantillo on 2023-02-09.
+//
+
+import SwiftUI
+
+@main
+struct SqaureTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
